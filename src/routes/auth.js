9fc361bc80a2dbe1cc793e585f4f169a1c72d7ea
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-28 23:26:12 +0800

    Configured auth.js.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,12 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const { credentials } = req.body;
-    User.findOne({ email: credentials.email })
+    User.findOne({ email: credentials.email }).then(user => {
+        if (user) {
+        } else {
+            res.status(400).json({ errors: { global: 'Invalid credentials' } });
+        }
+    });
 });
 
 export default router;
