01c46f92654ebc3cf0dac90149139b3558f6aae3
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-12 10:08:38 +0800

    Configured users.js in routes folder.

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,4 +2,9 @@ import React from 'react';
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    const { email, password } = req.body.user;
+    const user = new User ({ email });
+})
+
 export default router;
