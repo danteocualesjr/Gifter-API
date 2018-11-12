dbe358065289461484b00da4803becf41e9fbfc6
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-12 11:17:43 +0800

    Configured router.post in users.js.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import User from '../models/User';
-
+import parseErrors from '../utils/parseErrors';
 
 const router = express.Router();
 
@@ -8,7 +8,10 @@ router.post('/', (req, res) => {
     const { email, password } = req.body.user;
     const user = new User ({ email });
     user.setPassword(password);
-    user.save().catch(err => res.json({ err }));
+    user
+        .save()
+        .then(userRecord => res.json({ user: userRecord.toAuthJSON() }))
+        .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
 });
 
 export default router;
