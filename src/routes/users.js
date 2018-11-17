45694e5645694ed78bd0c0e7aae0ac7f1c743685
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-17 15:19:20 +0800

    Configured router.post in users.js.

--- src/routes/users.js
+++ src/routes/users.js
@@ -20,7 +20,13 @@ router.post('/', (req, res) => {
 
 router.post('/confirmation', (req, res) => {
     const token = req.body.token;
-    User.findOneAndUpdate({ confirmationToken: token }, { confirmationToken: "", })
-})
+    User.findOneAndUpdate(
+        { confirmationToken: token }, 
+        { confirmationToken: "", confirmed: true },
+        { new: true }
+    ).then(user =>
+        user ? res.json({ user: user.toAuthJSON() }) : res.status(400).json({})
+    );
+});
 
 export default router;
