99a3e6dfc16e266e8ec97cab3f1159731941c4bf
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-11-25 22:45:53 +0800

    Changed expiresIn to 2 hours.

--- src/models/User.js
+++ src/models/User.js
@@ -55,7 +55,7 @@ schema.methods.generateResetPasswordToken = function generateResetPasswordToken(
             _id: this._id
         },
         process.env.JWT_SECRET,
-        { expiresIn: "1h" }
+        { expiresIn: "2h" }
     );
 };
 
