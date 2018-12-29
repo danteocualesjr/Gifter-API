c5a5de447e035eb6edcdb272c4d3f7affb531a59
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-30 02:53:18 +0800

    Added get and then requests.

--- src/routes/gifts.js
+++ src/routes/gifts.js
@@ -19,7 +19,25 @@ router.post("/", (req, res) => {
 });
 
 router.get("/search", (req, res) => {
-
+    request
+        .get(
+            `https://www.goodreads.com/search/index.xml?key=${process.env
+            .GOODREADS_KEY}&q=${req.query.q}`
+        )
+        .then(result =>
+            parseString(result, (err, giftResult) =>
+            res.json({
+                gifts: giftResult.GiftResponse.search[0].results[0].work.map(
+                    work => ({
+                        giftId: work.best_book[0].id[0]._,
+                        title: work.best_book[0].title[0],
+                        authors: work.best_book[0].author[0].name[0],
+                        covers: [work.best_book[0].image_url[0]]
+                    })
+                )
+            })
+            )
+        );
 });
 
 export default router;
