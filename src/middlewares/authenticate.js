af1db402dc8d91c1cb7389b7d36bd3767b22a66c
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-29 10:43:31 +0800

    Configured authenticate.js.

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -8,6 +8,17 @@ export default (req. res, next) => {
     if (header) token = header.split("")[1];
 
     if (token) {
-        
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ errors: { global: "Invalid token" } });
+            } else {
+                User.findOne({ email: decoded.email }).then(user => {
+                    req.currentUser = user;
+                    next();
+                });
+            }
+        });
+    } else {
+        res.status(401).json({ errors: { global: "No token" } });
     }
-}
+};
