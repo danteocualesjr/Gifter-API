06405f5eace153233d773d7d9f71d3ed610240ec
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-12-30 02:48:50 +0800

    Configured endpoint for search.

--- src/routes/gifts.js
+++ src/routes/gifts.js
@@ -19,28 +19,7 @@ router.post("/", (req, res) => {
 });
 
 router.get("/search", (req, res) => {
-    res.json({
-        gifts: [
-            {
-                giftId: 1,
-                type: "book",
-                title: "Don't Let the Pigeon Drive the Bus",
-                author: "---",
-            },
-            {
-                giftId: 2,
-                type: "book",
-                title: "This is Not My Hat",
-                author: "---",
-            },
-            {
-                giftId: 3,
-                type: "book",
-                title: "I Sold my Dad for 12 Goldfish",
-                author: "Neil Gaiman",
-            },                        
-        ]
-    })
+
 });
 
 export default router;
